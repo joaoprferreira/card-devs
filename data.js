8a805383cbe4e618b521c0ade3d4b27e8ad6c6d3
Daniel Oliveira <daniel_oliveira110@hotmail.com>
2019-09-05 02:08:53 -0300

    function bildheader

---
+++ data.js
@@ -0,0 +1,72 @@
+const data = {
+    "id": 1,
+    "cnpj": "01.000.000/0000-01",
+    "nome": "Drogaria Devs",
+    "telefones": ["2120-3434", "2020-3030"],
+    "endereco": {
+      "rua": "Rua do Joao",
+      "numero": 22,
+      "cep": "22.320-980",
+      "complemento": "casa 3",
+      "bairro": "Guaratiba",
+      "cidade": "Rio de Janeiro",
+      "estado": "RJ"
+    },
+    "produtos": [
+      {
+        "id": 1,
+        "codigo_barra": "0120129356",
+        "nome": "Shampoo Dove",
+        "preco": 8,
+        "precisa_receita": false,
+        "categoria": "dermo",
+        "imagem_url": "https://www.dove.com/content/dam/unilever/dove/brazil/pack_design/3d/hair_care/wash_and_care/shampoo_dove_ritual_de_repara%C3%A7%C3%A3o_200_ml/ritual_de_reparacao_200ml_sh_8_fop_simples-864272-png.png.ulenscale.460x460.png"
+      },
+      {
+        "id": 2,
+        "codigo_barra": "0120128742",
+        "nome": "Shampoo Elseve",
+        "preco": 6,
+        "precisa_receita": false,
+        "categoria": "dermo",
+        "imagem_url": "https://www.drogariacatarinense.com.br/BACKOFFICE/Uploads/Produto/Ampliada/56558.jpg"
+      }
+    ],
+    "clientes": [
+      {
+        "nome": "Joao Silva",
+        "telefone": "91113-8343",
+        "cpf": "333.333.333-44",
+        "data_nascimento": "19/05/2000",
+        "endereco": {
+          "rua": "Rua do Joao",
+          "numero": 27,
+          "cep": "23.473-908",
+          "complemento": "casa 1",
+          "bairro": "Guaratiba",
+          "cidade": "Rio de Janeiro",
+          "estado": "RJ"
+        }
+      }, 
+      {
+        "nome": "Maria Queiroz",
+        "telefone": "97252-8466",
+        "cpf": "24.986.452-44",
+        "data_nascimento": "18/02/1998",
+        "endereco": {
+          "rua": "Rua da Maria",
+          "numero": 3,
+          "cep": "26.134-389",
+          "complemento": "casa 7",
+          "bairro": "Guaratiba",
+          "cidade": "Rio de Janeiro",
+          "estado": "RJ"
+        }
+      }
+    ],
+    "tamanho": "5",
+    "filial": "",
+    "funcionarios": "",
+    "logo_url": "https://www.designevo.com/res/templates/thumb_small/green-circle-capsule-and-cross.png"
+  }
+  
---
+++ main.js
@@ -0,0 +1,14 @@
+function buildPage(){
+    buildHeader();  
+}
+
+    function buildHeader(){
+
+        const headerContainer = document.querySelector("header");
+
+        headerContainer.innerHTML = `
+        <h1>${data.nome}</h1>
+        `
+    }
+     
+    buildPage();
