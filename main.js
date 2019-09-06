d1c7232165ba10bb97158e2e3cb5419638a6e570
Lubaaenzin <lubaaenzin@gmail.com>
2019-09-06 18:21:02 -0300

    produtos e clientes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function buildPage(){
     buildHeader();  
     buildSection();
     buildProdutos();
+    buildClientes();
 }
 
     function buildHeader(){
@@ -23,23 +24,39 @@ function buildPage(){
         
     function buildProdutos(){
         const cardsProdutos = document.querySelector(".cards")
-
-        cardsProdutos.innerHTML = `
-        <h1>Produtos</h1>
-        <div class="box">
-        <div class="card">
-        <img src="${data.produtos[0].imagem_url}" alt="" srcset="">
-        <h2>${data.produtos[0].nome}</h2>
-        <h5>R$${data.produtos[0].preco}</h5>
-        </div>
-
-        <div class="card">
-        <img src="${data.produtos[1].imagem_url}" alt="" srcset="">
-        <h2>${data.produtos[1].nome}</h2>
-        <h5>R$${data.produtos[1].preco}</h5>
-        </div>
-        </div>
-        `
+        var produtos = data.produtos;
+        var text = "";
+        var i;
+        for (i = 0, len = produtos.length; i < len; i++) {
+            text += `
+            <div class="box">
+            <div class="card">
+            <img src="${produtos[i].imagem_url}">
+            <h1>${produtos[i].nome}</h1>
+            <h4>R$${produtos[i].preco}<h4>
+            </div>
+            </div>`;
+          }
+        cardsProdutos.innerHTML = text;
+        
+    }
+    function buildClientes(){
+        const cardsClientes = document.querySelector(".cards_clientes")
+        var clientes = data.clientes;
+        var text = "";
+        var i;
+        for (i = 0, len = clientes.length; i < len; i++) {
+            text += `
+            <div class="box">
+            <div class="card">
+            <h1>${clientes[i].nome}</h1>
+            <h2>${clientes[i].cpf}</h2>
+            <h4>R$${clientes[i].telefone}<h4>
+            </div>
+            </div>`;
+          }
+        cardsClientes.innerHTML = text;
+        
     }
         
     
