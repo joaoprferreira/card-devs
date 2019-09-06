e453be087b9f42577f9951ef8d899f38906df100
joaoprferreira <joaof082@gmail.com>
2019-09-06 20:39:36 -0300

    reaponsivo

--- main.js
+++ main.js
@@ -27,12 +27,12 @@ function buildPage(){
         var i;
         for (i = 0, len = produtos.length; i < len; i++) {
             text += `
-            <div class="box">
-            <div class="card">
-            <img src="${produtos[i].imagem_url}">
-            <h1>${produtos[i].nome}</h1>
-            <h4>R$${produtos[i].preco}<h4>
-            </div>
+            <div class="box-products">
+                <div class="card">
+                    <img src="${produtos[i].imagem_url}">
+                    <h1>${produtos[i].nome}</h1>
+                    <h4>R$${produtos[i].preco}<h4>
+                </div>
             </div>`;
           }
         cardsProdutos.innerHTML = text;
@@ -46,12 +46,12 @@ function buildPage(){
         for (i = 0, len = clientes.length; i < len; i++) {
             text += `
             <div class="box">
-            <div class="card">
-            <h1>${clientes[i].nome}</h1>
-            <h2>CPF: ${clientes[i].cpf}</h2>
-            <h4>Telefone: ${clientes[i].telefone}<h4>
-            </div>
-            </div>`;
+                <div class="card">
+                    <h1>${clientes[i].nome}</h1>
+                    <h2>CPF: ${clientes[i].cpf}</h2>
+                    <h4>Telefone: ${clientes[i].telefone}<h4>
+             </div>
+            </div>`
           }
         cardsClientes.innerHTML = text;
         
